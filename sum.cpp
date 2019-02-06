06a4893ef2eff7bba8316b49054bb502e93e4c95
chloegottlieb <40703612+chloegottlieb@users.noreply.github.com>
2019-02-06 16:01:18 -0500

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 int main() {
-    float num, sum;
-    sum = 0;
+    float num, sum; //initializing variables as floats
+    sum = 0; //setting sum equal to zero, to be added onto later
     while(std::cin >> num) { // While the reading operation is a success
         //std::cout << num << std::endl;  // print the read word
         sum += num; // add number from txt file to total sum
         
     }
+    //end of while loop
     std::cout << sum << std::endl; //print final sum
 }
